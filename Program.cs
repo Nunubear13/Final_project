d4283c0caea2cf1af621abd9befd562512a8373c
Katie <eelebedeva89@gmail.com>
2023-01-16 16:06:02 +0200

    Saving code

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter text: ");
+string[] myProject = new string[] {"Hello", "GeekBrains", "16", "01", "2023"}; 
+for (int i = 0; i < myProject.Length; i++)
+{
+    if (myProject[i].Length <= 3)
+    {
+       Console.Write(myProject[i] + " ");
+    }
+
+}
